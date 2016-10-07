6f91f76eb8a2636b30450a2a2dbc6025540d3205
lmandlyp163 <18883288120@163.com>
2016-10-07 15:23:28 +0800

    ES

--- src/main/java/ibmdw/nio/WriteSomeBytes.java
+++ src/main/java/ibmdw/nio/WriteSomeBytes.java
@@ -8,16 +8,21 @@ public class WriteSomeBytes
 {
   static private final byte message[] = { 83, 111, 109, 101, 32,
                                           98, 121, 116, 101, 115, 46 };
-
+  //写一些数据进文件中
   static public void main( String args[] ) throws Exception {
-    FileOutputStream fout = new FileOutputStream( "writesomebytes.txt" );
+    FileOutputStream fout = new FileOutputStream( "e://writesomebytes.txt" );
 
     FileChannel fc = fout.getChannel();
 
-    ByteBuffer buffer = ByteBuffer.allocate( 1024 );
+    ByteBuffer buffer = ByteBuffer.allocate( 128 );
 
-    for (int i=0; i<message.length; ++i) {
-      buffer.put( message[i] );
+    for (int i=0; i<222; ++i) {
+    	if(buffer.hasRemaining()){
+    	    buffer.flip();
+    	    fc.write( buffer );
+    	    buffer.clear();
+    	}
+      buffer.put( ("ssd"+i+"---+"+"\n").getBytes() );
     }
 
     buffer.flip();
